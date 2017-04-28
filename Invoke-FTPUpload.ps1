a85ba022756eebacd3a99353374ce5ec3c3f54a1
nerbix <nick.r.britton@gmail.com>
2017-04-28 13:05:17 -0500

    initial

---
+++ Invoke-FTPUpload.ps1
@@ -0,0 +1,16 @@
+﻿function Invoke-FTPUpload {
+    param ([string] $dir, [string] $user, [string] $server, [string] $password)
+    
+
+#ftp server   
+
+$webclient = New-Object System.Net.WebClient 
+$webclient.Credentials = New-Object System.Net.NetworkCredential($user,$password)  
+
+Foreach($item in (dir $dir)){ 
+    "Uploading $item..."  
+    $uri = New-Object System.Uri($server+$item.Name) 
+    $webclient.UploadFile($uri, $item.FullName) 
+
+ } 
+}
